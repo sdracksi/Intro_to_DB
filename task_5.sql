a13ce79ce078f95542c44a84446cf4987e8bde84
Shadrack Sithole <shadrackdrack@gmail.com>
2025-02-16 18:34:04 +0200

    Correction

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
-INSERT INTO customers (customer_id, first_name, last_name, email, address) 
+INSERT INTO customer (customer_id, first_name, last_name, email, address) 
 VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
